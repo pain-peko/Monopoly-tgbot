03cf145ff6ccd0d2cf0c8f2392bcb980b7adc59d
SurveyZerg <50334084+SurveyZerg@users.noreply.github.com>
2019-10-20 14:44:48 +0200

    now it works

--- SourceCode/Commands.cs
+++ SourceCode/Commands.cs
@@ -7,23 +7,27 @@ using Telegram.Bot.Args;
 
 namespace Monopoly_tgbot
 {
+    [Serializable]
     abstract class Command
     {
-        public string Name { get; private set; }
-        public bool IsActivated { get; protected set; }
+        public string Name { get; set; }
+        public bool IsActivated { get; set; }
         public Command(string name, bool isActivated)
         {
             Name = name;
             IsActivated = isActivated;
         }
+        public Command() { }
     }
+    [Serializable]
     class ActivatedCommand : Command
     {
-        public long Activator { get; private set; }
+        public long Activator { get; set; }
         public ActivatedCommand(string name, MessageEventArgs args) : base(name, true)
         {
             Activator = args.Message.Chat.Id;
         }
+        public ActivatedCommand() { }
     }
     class DisactivatedCommand : Command
     {
@@ -40,17 +44,42 @@ namespace Monopoly_tgbot
             try
             {
                 ActivatedCommandsList = JsonConvert.DeserializeObject<List<ActivatedCommand>>(File.ReadAllText(FilePath));
+
+                if (ActivatedCommandsList == null)
+                    ActivatedCommandsList = new List<ActivatedCommand>();
             }
             catch (FileNotFoundException)
             {
                 throw new ArgumentException("Файла Commands.json не существует");
             }
         }
-        ~Commands()
+        public void SaveCommands()
         {
             File.WriteAllText(FilePath, JsonConvert.SerializeObject(ActivatedCommandsList));
         }
 
+        public bool ContainsActivatedCommand (string name, MessageEventArgs args)
+        {
+            for (int i = 0; i < ActivatedCommandsList.Count(); i++)
+                if (ActivatedCommandsList[i].Name == name && ActivatedCommandsList[i].Activator == args.Message.Chat.Id)
+                    return true;
+
+            return false;
+        }
+        public bool ContainsActivatedCommand(MessageEventArgs args)
+        {
+            for (int i = 0; i < ActivatedCommandsList.Count(); i++)
+                if (ActivatedCommandsList[i].Activator == args.Message.Chat.Id)
+                    return true;
+
+            return false;
+        }
+        public void RemoveCommand (string name, MessageEventArgs args)
+        {
+            for (int i = 0; i < ActivatedCommandsList.Count(); i++)
+                if (ActivatedCommandsList[i].Name == name && ActivatedCommandsList[i].Activator == args.Message.Chat.Id)
+                    ActivatedCommandsList.RemoveAt(i);
+        }
         static public bool IsCommand(string text)
         {
             if (text != null && text[0] == '/')
