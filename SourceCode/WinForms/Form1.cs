7601ca1ab5196328a39520c33bd09873e582fed1
SurveyZerg <50334084+SurveyZerg@users.noreply.github.com>
2019-10-20 18:15:34 +0200

    Revert "Repaired Gamer construcctor"
    This reverts commit 31c325a1a1c9066b53e31df853a48b456c065f96.

--- SourceCode/WinForms/Form1.cs
+++ SourceCode/WinForms/Form1.cs
@@ -20,7 +20,7 @@ namespace Monopoly_tgbot
         public static TelegramBotClient Client;
 
 
-        public  static string usersPath = "Files/Users.json";
+        public string usersPath = "Files/Users.json";
 
         public const string SecretPassword = "Альфа Влад";
 
