a9890ce1c1f3c047f7e24f4c8540c01dfa519048
Andrii Hrytsenko <andrii@studiovector.pl>
2019-10-22 22:10:00 +0200

    Added ResetBtn

--- SourceCode/WinForms/Form1.Designer.cs
+++ SourceCode/WinForms/Form1.Designer.cs
@@ -31,7 +31,10 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.Log = new System.Windows.Forms.RichTextBox();
             this.StartStop = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Reset = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -39,22 +42,22 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.Log, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.StartStop, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.777778F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.22222F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.111111F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.88889F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // Log
             // 
             this.Log.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Log.Location = new System.Drawing.Point(3, 28);
+            this.Log.Location = new System.Drawing.Point(3, 43);
             this.Log.Name = "Log";
-            this.Log.Size = new System.Drawing.Size(794, 419);
+            this.Log.Size = new System.Drawing.Size(794, 404);
             this.Log.TabIndex = 0;
             this.Log.Text = "";
             // 
@@ -63,11 +66,37 @@
             this.StartStop.Dock = System.Windows.Forms.DockStyle.Fill;
             this.StartStop.Location = new System.Drawing.Point(3, 3);
             this.StartStop.Name = "StartStop";
-            this.StartStop.Size = new System.Drawing.Size(794, 19);
+            this.StartStop.Size = new System.Drawing.Size(373, 28);
             this.StartStop.TabIndex = 1;
             this.StartStop.Text = "Start";
             this.StartStop.UseVisualStyleBackColor = true;
             this.StartStop.Click += new System.EventHandler(this.StartStop_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.733F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.267F));
+            this.tableLayoutPanel2.Controls.Add(this.StartStop, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Reset, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 34);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // Reset
+            // 
+            this.Reset.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Reset.Location = new System.Drawing.Point(382, 3);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(409, 28);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
             // 
             // Form1
             // 
@@ -78,6 +107,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -87,6 +117,8 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.RichTextBox Log;
         private System.Windows.Forms.Button StartStop;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button Reset;
     }
 }
 
